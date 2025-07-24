100189901212636ca86860767bc70f91710cec08
choosedpeace <161508085+choosedpeace@users.noreply.github.com>
2025-07-25 03:17:04 +0530

    Add files via upload

---
+++ WEEK-6 React/1- ReactJS-HOL/CODE/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <center><h1>Welcome to the first session of React</h1></center>
+    </div>
+  );
+}
+
+export default App;
